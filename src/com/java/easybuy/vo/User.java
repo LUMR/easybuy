35e19f392458f5fe26e7d38ed922a45589fab83a
LUMR <freedomyes@sina.com>
2017-03-27 22:44:25 +0800

    create User

---
+++ src/com/java/easybuy/vo/User.java
@@ -0,0 +1,25 @@
+package com.java.easybuy.vo;
+
+/**
+ * Created by fsweb on 17-3-27.
+ */
+public class User {
+    private String name;
+    private String password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
